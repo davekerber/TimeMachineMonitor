2ba64a2ff10e71cdcc3b19cd831919cb59783aab
David Kerber <dave@agapered.com>
2013-07-20 14:14:53 -0500

    initiall commit of project, we are showing a menu bar icon

--- TimeMachineMonitor/AppDelegate.m
+++ TimeMachineMonitor/AppDelegate.m
@@ -13,6 +13,46 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
+//    NSStatusItem *statusItem;
+
+    NSStatusBar *statusBar = [NSStatusBar systemStatusBar];
+    self.statusItem = [statusBar statusItemWithLength:NSSquareStatusItemLength];
+    
+    if([self statusItem]) {
+//        NSLog(@"%@", [self statusItem]);
+        NSImage *theIconImage = [NSImage imageNamed:@"Icon1.png"] ;
+//        [self.statusItem setImage: theIconImage];
+//        [[self statusItem] setTitle:@"My Item"];
+        NSLog(@"%@", theIconImage);
+        [self.statusItem setToolTip:@"My Tooltip!"];
+
+        
+        
+        NSMenu *theMenu = [[NSMenu alloc] initWithTitle:@""];
+        [theMenu setAutoenablesItems:NO];
+        
+
+        
+
+        [theMenu addItemWithTitle:@"One" action:nil keyEquivalent:@""];
+        [theMenu addItemWithTitle:@"Two" action:nil keyEquivalent:@""];
+        [theMenu addItemWithTitle:@"Three" action:nil keyEquivalent:@""];
+        [theMenu addItem:[NSMenuItem separatorItem]];
+        NSMenuItem *tItem = [theMenu addItemWithTitle:@"Quit" action:@selector(handleQuit:) keyEquivalent:@"q"];
+        [tItem setKeyEquivalentModifierMask:NSCommandKeyMask];
+        
+        [self.statusItem setMenu:theMenu];
+        
+//        [NSThread sleepForTimeInterval: 1000];
+        [self.statusItem setImage: theIconImage];
+    }
+ 
+    
+    
+}
+
+-(void) handleQuit:(id) sender {
+    [NSApp terminate: self];
 }
 
 @end
